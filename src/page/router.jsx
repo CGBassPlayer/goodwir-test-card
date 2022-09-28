1396ebdd32ee394068a7dcd984ca69c91a64d229
Ryan Goodwin <goodwinryan2@gmail.com>
2022-09-28 10:58:40 -0400

    🔥 Remove page1 component

--- src/page/router.jsx
+++ src/page/router.jsx
@@ -6,7 +6,6 @@ import {
   Route
 } from "react-router-dom";
 import Home from "./Home";
-import Page1 from "./Page1";
 
 // for more information on react router: https://v5.reactrouter.com/web/guides/quick-start
 
@@ -16,9 +15,6 @@ const RouterPage = (props) => {
       <Switch>
         <Route path="/">
           <Home {...props} />
-        </Route>
-        <Route path="/page1">
-          <Page1 />
         </Route>
       </Switch>
     </Router>
